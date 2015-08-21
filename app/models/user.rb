e01a89d0853e2f35a5cf56105eb879eab06911a7
Katherine Walker <katewalker171@gmail.com>
2015-08-20 21:53:15 -0400

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
+
   attr_accessor :remember_token, :activation_token, :reset_token
   before_create :create_activation_digest
   before_save :downcase_email
@@ -66,6 +68,10 @@ class User < ActiveRecord::Base
   # Returns true if password reset has expired
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
+  end
+
+  def feed
+    Micropost.where("user_id = ?", id)
   end
 
   private
